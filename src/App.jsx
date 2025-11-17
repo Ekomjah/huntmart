2c0a4bf00d52e5d2301c569492709dd40cf290d4
Ekomjah <ekomjahedet@gmail.com>
2025-11-17 04:29:04 +0100

    style(carousel): add full width to the hero img banners and a text layer too

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,27 @@ function App() {
   return (
     <>
       <PrimaryAppBar />
-      <div className="mt-[75px]">
+      <div className="relative mt-[65px]">
         <CarouselApp />
+        <div className="absolute top-1/2 z-60 -translate-y-1/2 rounded-r-full bg-(--hunt-bg) p-4 pr-6 text-red-400">
+          <h2 className="font-sans font-semibold">Welcome to</h2>
+          <h1 className="font-pop text-4xl! font-bold lg:text-8xl!">
+            {" "}
+            Hunt Mart
+          </h1>
+          <h2 className="m-0 p-0 font-sans text-base font-normal">
+            A Shopping and Department Store
+          </h2>
+          <p className="font-sans text-base font-semibold lg:text-lg">
+            Every shopper is a hunter. Every basket is a trophy.
+          </p>
+        </div>
       </div>
-      <div style={{ padding: "2rem" }}>
+
+      <div>
+        Shop our various categories
+      </div>
+      <div style={{ padding: "1rem" }}>
         <h1>Hello Theme!</h1>
         <p>The theme is now applied to your document.</p>
       </div>
--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,115 +1,85 @@
-import React, { useRef, useState, useEffect } from "react";
-import { CirclePlay, CirclePause } from "lucide-react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import axios from "axios";
-import img1 from "../../assets/pictures/charlesdeluvio-FK81rxilUXg-unsplash.jpg";
-import img2 from "../../assets/pictures/freestocks-_3Q3tsJ01nc-unsplash.jpg";
-import img3 from "../../assets/pictures/freestocks-VFrcRtEQKL8-unsplash.jpg";
-import img4 from "../../assets/pictures/img.avif";
+  import React, { useRef, useState } from "react";
+  import { CirclePlay, CirclePause } from "lucide-react";
+  import { Swiper, SwiperSlide } from "swiper/react";
+  import img1 from "../../assets/pictures/charlesdeluvio-FK81rxilUXg-unsplash.jpg";
+  import img2 from "../../assets/pictures/freestocks-_3Q3tsJ01nc-unsplash.jpg";
+  import img3 from "../../assets/pictures/freestocks-VFrcRtEQKL8-unsplash.jpg";
+  import img4 from "../../assets/pictures/maria-lin-kim-8RaUEd8zD-U-unsplash.jpg";
+  import img5 from "../../assets/pictures/nathan-waters-ykH4Wtocefc-unsplash.jpg";
+  import { cn } from "../../lib/utils";
+  // Import Swiper styles
+  import "swiper/css";
+  import "swiper/css/pagination";
+  import "swiper/css/navigation";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+  import styles from "./styles.module.css";
 
-import "./styles.css";
+  // import required modules
+  import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
-// import required modules
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+  export default function CarouselApp() {
+    const img = [img1, img2, img3, img4, img5];
+    const [isPlaying, setIsPlaying] = useState(true);
+    const progressCircle = useRef(null);
+    const swiperRef = useRef(null);
+    const progressContent = useRef(null);
+    const onAutoplayTimeLeft = (s, time, progress) => {
+      progressCircle.current.style.setProperty("--progress", 1 - progress);
+      // progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    };
 
-export default function CarouselApp() {
-  const [img, setImg] = useState([img1, img2, img3, img4]);
-  useEffect(() => {
-    const urls = [
-      "https://fakestoreapi.com/products",
-      "https://dummyjson.com/products",
-    ];
+    const handlePlaynPause = () => {
+      if (!swiperRef.current) return;
 
-    async function getCarouselImages() {
-      const res = await axios.get(urls[0]);
-      const arr = res.data.map((el) => el.image);
-      return arr;
-    }
-    async function getOtherImages() {
-      const res = await axios.get(urls[1]);
-      const arr = res.data.products.map((el) => el.images[0]);
-      return arr;
-    }
-    Promise.all([getCarouselImages(), getOtherImages()]).then(
-      ([img, otherImg]) => {
-        const arr = [...img, ...otherImg];
-        const index = [];
-        for (let i = 0; i < 10; i++) {
-          index.push(Math.floor(Math.random() * arr.length));
-        }
-        const setterArr = index.map((el) => arr[el]);
-        console.log(setterArr);
-        console.log(index);
-        setImg((prev) => [...prev, setterArr]);
-      },
+      if (isPlaying) {
+        swiperRef.current.autoplay.stop();
+      } else {
+        swiperRef.current.autoplay.start();
+      }
+
+      setIsPlaying(!isPlaying);
+    };
+
+    return (
+      <>
+        <Swiper
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          onAutoplayTimeLeft={onAutoplayTimeLeft}
+          className={cn(styles.mySwiper, "relative")}
+        >
+          {img.map((image) => (
+            <SwiperSlide key={image}>
+              <div className={cn(styles["slide-inner"], "h-full w-full")}>
+                <img src={image} className="w-full" />
+              </div>
+            </SwiperSlide>
+          ))}
+          <div className={styles["autoplay-progress"]} slot="container-end">
+            <svg viewBox="0 0 48 48" ref={progressCircle}>
+              <circle cx="24" cy="24" r="20"></circle>
+            </svg>
+            <span ref={progressContent}></span>
+          </div>
+          <div className={styles.controls}>
+            <div className={styles.control} onClick={handlePlaynPause}>
+              {isPlaying ? (
+                <CirclePause color="red" />
+              ) : (
+                <CirclePlay color="red" />
+              )}
+            </div>{" "}
+          </div>
+        </Swiper>
+      </>
     );
-  }, []);
-  const [isPlaying, setIsPlaying] = useState(true);
-  const progressCircle = useRef(null);
-  const swiperRef = useRef(null);
-  const progressContent = useRef(null);
-  const onAutoplayTimeLeft = (s, time, progress) => {
-    progressCircle.current.style.setProperty("--progress", 1 - progress);
-    // progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-  };
-
-  const handlePlaynPause = () => {
-    if (!swiperRef.current) return;
-
-    if (isPlaying) {
-      swiperRef.current.autoplay.stop();
-    } else {
-      swiperRef.current.autoplay.start();
-    }
-
-    setIsPlaying(!isPlaying);
-  };
-
-  return (
-    <>
-      <Swiper
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
-        className="mySwiper relative"
-      >
-        {img.map((image) => (
-          <SwiperSlide>
-            <div className="h-full w-[90vw]">
-              <img src={image} className="w-full object-contain" />
-            </div>
-          </SwiperSlide>
-        ))}
-        <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="24" cy="24" r="20"></circle>
-          </svg>
-          <span ref={progressContent}></span>
-        </div>
-        <div className="controls">
-          <div className="control" onClick={handlePlaynPause}>
-            {isPlaying ? (
-              <CirclePause color="red" />
-            ) : (
-              <CirclePlay color="red" />
-            )}
-          </div>{" "}
-        </div>
-      </Swiper>
-    </>
-  );
-}
+  }
