63bc338b2f61ab76f1b160bb88307cac616ddaa7
Ekomjah <ekomjahedet@gmail.com>
2025-11-19 15:53:29 +0100

    fix(app-bar): fix the stale input element

--- src/components/AppBar/appBar.jsx
+++ src/components/AppBar/appBar.jsx
@@ -50,7 +50,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: "0ch",
+      width: "50ch",
     },
   },
 }));
