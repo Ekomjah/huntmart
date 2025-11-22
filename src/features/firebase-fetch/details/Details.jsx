52ef9a6c218ee1cdd65a0a072c8390b7ce028059
Ekomjah <ekomjahedet@gmail.com>
2025-11-22 16:54:59 +0100

    style(details): enhance products details display

--- src/features/firebase-fetch/details/Details.jsx
+++ src/features/firebase-fetch/details/Details.jsx
@@ -17,32 +17,53 @@ function Details() {
   } = detailsObj;
   return (
     <>
-      <div>
-        <h2>Brand: {brand ?? "N/A"}</h2>
-        <p>SKU: {sku ?? "N/A"}</p>
-        <p>Weight: {weight ?? "N/A"} kg</p>
-        <p>{category ?? "N/A"}</p>
-        <div>
-          <h2>Dimensions</h2>
+      <h2 className="font-pop m-4 text-4xl font-semibold">Product Details</h2>
+      <div className="flex flex-col gap-2 text-left">
+        <div className="flex gap-2">
+          <h2 className="text-base font-semibold text-gray-800">Brand:</h2>
+          <b>
+            <i>{brand ?? "N/A"}</i>
+          </b>
+        </div>
+        <p className="text-base font-semibold text-gray-800">
+          <span>Sku:</span> <span>{sku ?? "N/A"}</span>
+        </p>
+        <p className="text-base font-semibold text-gray-800">
+          Weight: {weight ?? "N/A"} kg
+        </p>
+        <p className="text-base font-semibold text-gray-800">
+          Category: {category ?? "N/A"}
+        </p>
+        <div className="flex gap-2 text-base font-semibold text-gray-800">
+          <h2>Dimensions: </h2>
           {dimensions ? (
             <div>
-              <p>Depth: {dimensions.depth}</p>
-              <p>Height: {dimensions.height}</p>
-              <p>Width: {dimensions.width}</p>
+              <p>
+                {dimensions.depth} * {dimensions.height} * {dimensions.width}
+              </p>
             </div>
           ) : (
             <p>N/A</p>
           )}
-        </div>
-
-        <p>Warranty: {warrantyInformation}</p>
-        <p>Shipping: {shippingInformation}</p>
-        <p>Status: {availabilityStatus}</p>
-        <p>Return Policy: {returnPolicy}</p>
-        <p>Minimum Order: {minimumOrderQuantity}</p>
-        <div className="mt-4 text-2xl font-semibold">Scan Here:</div>
-        <BarcodeGenerator meta={meta} />
+        </div>{" "}
+        <p className="flex gap-2 text-base font-semibold text-gray-800">
+          <span>Warranty:</span> {warrantyInformation}
+        </p>
+        <p className="flex gap-2 text-base font-semibold text-gray-800">
+          <span>Shipping:</span> {shippingInformation}
+        </p>
+        <p className="flex gap-2 text-base font-semibold text-gray-800">
+          <span>Status:</span> {availabilityStatus}
+        </p>
+        <p className="flex gap-2 text-base font-semibold text-gray-800">
+          <span>Return Policy:</span> {returnPolicy}
+        </p>
+        <p className="flex gap-2 text-base font-semibold text-gray-800">
+          <span>Minimum Order:</span> {minimumOrderQuantity}
+        </p>
       </div>
+      <div className="mt-4 text-2xl font-semibold">Scan Here:</div>
+      <BarcodeGenerator meta={meta} />
     </>
   );
 }
